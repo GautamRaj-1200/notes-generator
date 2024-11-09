dd1adffba2acd057bda690a88e57fb54d6060054
GautamRaj-1200 <gautamrajv2@gmail.com>
2024-11-09 12:25:32 +0530

    feat: add basic navbar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <>
+      <header className="header">
+        <nav className="navbar">
+          <h2 className="navbar__heading">
+            <a href="/">Notes Generator</a>
+          </h2>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
