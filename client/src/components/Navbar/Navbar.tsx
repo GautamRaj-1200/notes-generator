04cf15779fafd2be05f564122fbceccf735f719d
GautamRaj-1200 <gautamrajv2@gmail.com>
2024-11-15 14:20:33 +0530

    feat: add navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -6,8 +6,44 @@ const Navbar = () => {
       <header className="header">
         <nav className="navbar">
           <h2 className="navbar__heading">
-            <a href="/">Notes Generator</a>
+            <a href="/" className="nav-link">
+              Notes Generator
+            </a>
           </h2>
+          <ul>
+            <li>
+              <a href="#" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <ul className="nav-right">
+            <li>
+              <a href="#" className="nav-link sign-in-link">
+                SignIn With Google
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link report-link">
+                Report an issue
+              </a>
+            </li>
+          </ul>
         </nav>
       </header>
     </>
